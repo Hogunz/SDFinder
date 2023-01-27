b6a2ccd6168ae1f403655e59270b91085db718b8
Hogunz <cjdc1414@gmail.com>
2023-01-27 17:02:42 +0800

    1/27/2023

--- resources/views/vendors/phones/create.blade.php
+++ resources/views/vendors/phones/create.blade.php
@@ -11,7 +11,8 @@
 
                 <div class="mt-2">
                     <x-label for="brand_id" value="Select Brand" />
-                    <x-select name="brand_id" id="brand_id" x-model="brand_id" @change="selectBrand" class="w-full block rounded">
+                    <x-select name="brand_id" id="brand_id" x-model="brand_id" @change="selectBrand"
+                        class="w-full block rounded">
                         <option value="" hidden>Select Brand</option>
                         <template x-for="(brand, index) in brands" :key="index">
                             <option :value="brand.id" x-text="brand.name"></option>
@@ -21,7 +22,8 @@
 
                 <div class="mt-2" x-show="brand_id != ''">
                     <x-label for="phone_id" value="Select Phone" />
-                    <x-select name="phone_id" id="phone_id" x-model="phone_id" @change="selectPhone" class="w-full block rounded">
+                    <x-select name="phone_id" id="phone_id" x-model="phone_id" @change="selectPhone"
+                        class="w-full block rounded">
                         <option value="" hidden>Select Phone</option>
                         <template x-for="(phone, index) in phones" :key="index">
                             <option :value="phone.id" x-text="phone.name"></option>
@@ -37,14 +39,16 @@
                     <x-select name="variant" id="variant" x-model="variant" class="w-full block rounded">
                         <option value="" hidden>Select Varant</option>
                         <template x-for="(variant, index) in variants" :key="index">
-                            <option :value="JSON.stringify(variant)" x-text="variant.ram+` GB,`+variant.storage+` GB`"></option>
+                            <option :value="JSON.stringify(variant)" x-text="variant.ram+` GB,`+variant.storage+` GB`">
+                            </option>
                         </template>
                     </x-select>
                 </div>
 
                 <div class="mt-2" x-show="variant != ''">
                     <x-label for="price" value="Price" />
-                    <x-input id="price" type="number" name="price" x-model.number="price" class="w-full block rounded"></x-input>
+                    <x-input id="price" type="number" step="0.01" name="price" x-model.number="price"
+                        class="w-full block rounded"></x-input>
                 </div>
 
                 <div class="mt-2" x-show="price != 0">
@@ -58,8 +62,7 @@
 </x-guest-layout>
 
 <script>
-    function phones()
-    {
+    function phones() {
         return {
             brands: @json($brands),
             phones: [],
@@ -68,15 +71,13 @@
             phone_id: '',
             variant: '',
             price: 0,
-            selectBrand()
-            {
+            selectBrand() {
                 this.phone_id = ''
                 this.variant = ''
                 this.price = 0
                 this.phones = this.brands.filter((brand) => brand.id == this.brand_id)[0].phones
             },
-            selectPhone()
-            {
+            selectPhone() {
                 this.variant = ''
                 this.price = 0
                 this.variants = this.phones.filter((phone) => phone.id == this.phone_id)[0].variants
