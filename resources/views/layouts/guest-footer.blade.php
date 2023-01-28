b0b2bf0d7375eedfae57eea6e559c6628590791f
Hogunz <cjdc1414@gmail.com>
2023-01-28 09:18:15 +0800

    1/28/2023

--- resources/views/layouts/guest-footer.blade.php
+++ resources/views/layouts/guest-footer.blade.php
@@ -1,9 +1,11 @@
-
 <footer class=" dark:bg-gray-600 mix-blend-multiply">
- 
+
     <div class="py-6 px-4 bg-gray-100 dark:bg-gray-700  mix md:flex md:items-center md:justify-between">
-        <span class="text-sm text-gray-500 dark:text-gray-300 sm:text-center">© 2022 <a href="/">SDFinder™</a>. All Rights Reserved.
+        <span class="text-sm text-gray-500 dark:text-gray-300 sm:text-center">©
+            <script>
+                document.write(new Date().getFullYear());
+            </script><a href="/">SDFinder™</a>. All Rights Reserved.
         </span>
-      
+
     </div>
 </footer>
